27a3abe00e10f85ce0c04caed20d48b24ba3a1d1
mohla <donald.mxolisi@proton.me>
2024-08-24 17:40:38 +0200

    new:

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration'; 
+
 
 ReactDOM.render(
   <>
@@ -11,4 +11,4 @@ ReactDOM.render(
 );
 
 
-serviceWorkerRegistration.register();
+
